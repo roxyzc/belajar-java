b9ef270718f0d418b6ee338a980fb358479fa199
roxyzc <roxyzc12@gmail.com>
2022-09-19 16:59:54 +0700

    commit awal

---
+++ MyClass.java
@@ -0,0 +1,21 @@
+public class MyClass {
+    public static void main(String[] args) {
+        // String namaDepan = "roxyzc", namaBelakang = "zxocy", coba =
+        // "jkasdjabsjdbansdbajkakh";
+        // int Coba = coba.indexOf("a", 3);
+        // System.out.println(namaDepan.concat(namaBelakang));
+        // System.out.println(Coba);
+        // System.out.println(Coba);
+        // int math = (int) (Math.random() * 4);
+        // System.out.println(math);
+        // if (math == 1) {
+        // System.out.println("batu");
+        // return;
+        // }
+        // if (math == 2) {
+        // System.out.println("kertas");
+        // return;
+        // }
+        // System.out.println("gunting");
+    }
+}
