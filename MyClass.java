c048db7586546d647b4184fec22ec25ea3739b00
roxyzc <roxyzc12@gmail.com>
2022-09-19 17:44:22 +0700

    tambahan

--- MyClass.java
+++ MyClass.java
@@ -6,7 +6,7 @@ public class MyClass {
         // System.out.println(namaDepan.concat(namaBelakang));
         // System.out.println(Coba);
         // System.out.println(Coba);
-        // int math = (int) (Math.random() * 4);
+        // int math = (int) (Math.random() * 3 + 1);
         // System.out.println(math);
         // if (math == 1) {
         // System.out.println("batu");
@@ -16,6 +16,52 @@ public class MyClass {
         // System.out.println("kertas");
         // return;
         // }
+        // if (math > 3) {
+        // System.out.println("nggak boleh diatas 3");
+        // return;
+        // }
         // System.out.println("gunting");
+
+        // ternary
+        // user yang ngisi
+        // Boolean coba = true;
+        // coba ini diseleksi
+        // System.out.println(coba ? "bener" : "salah");
+
+        // int angka = 5;
+        // switch (angka) {
+        // case 1:
+        // System.out.println("ini 1");
+        // break;
+        // case 2:
+        // System.out.println("ini 2");
+        // break;
+        // default:
+        // System.out.println("default");
+        // break;
+        // }
+
+        // while (angka < 10) {
+        // System.out.println(angka);
+        // angka++;
+        // }
+
+        // do {
+        // System.out.println(angka);
+        // angka++;
+        // } while (angka < 10);
+
+        // for (int angka = 1; angka < 10; angka++) {
+        // if (angka == 5) {
+        // continue;
+        // }
+        // System.out.println(angka);
+        // }
+
+        String[] objek = { "roxyzc", "zxocy", "rozy", "manusia" };
+        System.out.println(objek[3]);
+        // for (String i : objek) {
+        // System.out.println(i);
+        // }
     }
 }
